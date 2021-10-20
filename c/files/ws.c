adf5a736a190be05a272b058da918b643679870f
dolev <dolev.arev@gmail.com>
2021-10-20 08:54:10 +0300

    dswww

--- c/files/ws.c
+++ c/files/ws.c
@@ -48,26 +48,148 @@ void Print(int i)
 
 void Logger(char *nameFile)
 {
-	char *str = NULL;
-	FILE *stream = NULL;
-	printf("write here\n");
-	CALLOC(str, 256);
 
-	while(*str != 's')
-	{
-		fgets(str, 256, stdin);
-		/*stream = fopen(nameFile, "a");*/
-		OPENFILE(stream, nameFile, "a");
-		
-		fprintf(stream,"%s", str);
-
-		fclose(stream);	
+	char *input = NULL;
+	FILE *fp = NULL;
+	int i;
+	const int NUM_OF_FUNCS = 5;
 	
+	typedef struct 
+	{	
+		char *str;
+		int (*CmpFunc)(char*, char*);
+		void (*OperationFunc)(FILE*)(char*)(char*);	
+	}funcLogger_t;	
+	
+	funcLogger_t FuncArray [NUM_OF_FUNCS]
+	
+	CALLOC(input, 256);
+	
+	FuncArray[0].str = "-remove";
+	FuncArray[0].CmpFunc = StrCmp;
+	FuncArray[0].OperationFunc = RemoveFile;	
+	
+	FuncArray[1].str = "-count";
+	FuncArray[1].CmpFunc = StrCmp;
+	FuncArray[1].OperationFunc = CountLines;	
+
+	FuncArray[2].str = "-exit";
+	FuncArray[2].CmpFunc = StrCmp;
+	FuncArray[2].OperationFunc = ExitProgram;	
+	
+	FuncArray[3].str = "<";
+	FuncArray[3].CmpFunc = isToFirst;
+	FuncArray[3].OperationFunc = AddFirstToFile;
+	
+	FuncArray[4].str = "";
+	FuncArray[4].CmpFunc = AlwaysTrue;
+	FuncArray[4].OperationFunc = WriteToFile;	
+	
+	printf("write here\n");
+	fgets(input, 256, stdin);
+	
+	while(*input != 's')
+	{
+	
+
+		OPENFILE(fp, nameFile, "a");
+		
+		for(i = 0; i < NUM_OF_FUNCS, ++i)
+		{
+			if( 0 == FuncArray[i].CmpFunc(FuncArray[i].str, input)
+			{
+				FuncArray[i].OperationFunc(fp, nameFile, input);
+				break;
+			}	
+		}
+		fgets(input, 256, stdin);
 	}
-	FREE(str);
+	
+
+	FREE(input);
 }
 
+int StrCmp(const char* str1, const char* str2)
+{ 
+   assert(str1);
+   assert(str2);
+  
+   while(*str1){ 
+      if(*str1 != *str2)
+      {
+         break;
+      }
+      str1++;
+      str2++;
+   }
+   return *str1 - *str2;
+}
 
+int isToFirst(const char* str1, const char* str2)
+{
+	return !(*str1 == '<');
+}
+
+int AlwaysTrue(const char* str1, const char* str2)
+{
+	return 0;
+}
+
+STATUS_FUNC RemoveFile(FILE* fp, char *FileName, char *input)
+{	
+	fclose(fp);	
+	remove(FileName);
+	return SUCCESS_t;
+}
+
+STATUS_FUNC CountLines(FILE* fp, char *FileName, char *input)
+{
+    printf("%d", HowManyLines(fp));
+    fclose(fp);	
+    return SUCCESS_t;
+}
+
+STATUS_FUNC ExitProgram(FILE* fp, char *FileName, char *input)
+{
+	fclose(fp);
+	FREE(input);	
+	exit(1);
+    return SUCCESS_t;
+}
+
+STATUS_FUNC AddFirstToFile(FILE* fp, char *FileName, char *input)
+{
+	
+	fseek(fp, 0, SEEK_SET);
+	fprintf(fp,"%s", ++input);
+	fseek(fp, 0, SEEK_END);
+	
+	
+	fclose(fp);
+	return SUCCESS_t;	
+}
+
+STATUS_FUNC WriteToFile(FILE* fp, char *FileName, char *input)
+{
+	fprintf(fp,"%s", input);
+	fclose(fp);
+	return SUCCESS_t;	
+}
+
+int HowManyLines(FILE* fp)
+{
+	char ch = '\0';
+	int lines = 0;
+	while(!feof(fp))
+	{
+	  ch = fgetc(fp);
+	  if(ch == '\n')
+	  {
+	    lines++;
+	  }
+	}
+	return lines;
+}
 
 
 
