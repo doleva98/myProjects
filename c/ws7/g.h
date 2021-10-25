7f6a937c14b78d1750c62d19cbc0da81af536051
dolev <dolev.arev@gmail.com>
2021-10-25 20:13:33 +0300

    ds

---
+++ c/ws7/g.h
@@ -0,0 +1,6 @@
+#ifndef G_H
+#define G_H
+
+static int g_s = 3;
+
+#endif
---
+++ c/ws7/main.c
@@ -0,0 +1,12 @@
+#include "g.h"
+#include <stdio.h>
+
+int main()
+{
+
+	printf("%d", g_s);
+	Foo();
+	printf("%d", g_s);
+
+	return 0;
+}
