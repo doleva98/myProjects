3d77157af7a127be06bd33ff7f2dd5fe9927b330
dolev <dolev.arev@gmail.com>
2021-11-03 15:14:44 +0200

    fdf

---
+++ ds/stack/stack.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "stack.h"
+
+struct Stack
+{
+	int capacity;
+	int top;
+	void **a;
+};
+
+
+/*Return a new stack*/
+extern Stack_t *StackCreate(size_t capacity)
+{
+	Stack_t *stack = NULL;
+	
+	stack = (Stack_t*)calloc(1, sizeof(Stack_t));
+	if(!stack)
+	{
+		return NULL;
+	}
+	stack->capacity = capacity;
+	stack->top = 0;
+	stack->a = (void**)calloc(capacity, sizeof(void**));
+	return stack;
+}
+
+/*Delete the stack */
+extern void StackDestroy(Stack_t *stack)
+{	
+	free(stack->a);
+	stack->a = NULL;
+	free(stack);
+	stack = NULL;
+}
+
+/*Add a new element to the top of the stack */
+extern void StackPush(Stack_t *stack, void* new_element)
+{
+	++stack->top;
+	stack->a[stack->top] = new_element;
+}
+
+/*Remove the last element */
+extern void StackPop(Stack_t *stack)
+{
+	--stack->top;
+}
+
+/*Return the value of the last element */
+extern void* StackPeek(Stack_t *stack)
+{
+	return stack->a[stack->top];
+}
+
+/*Check if the array is empty, Return 0 or 1*/
+extern int StackIsEmpty(Stack_t *stack)
+{
+	return stack->top == 0;
+}
+
+/*Return how many elements are used*/
+extern size_t StackSize(Stack_t *stack)
+{
+	return stack->top;
+}
+
+/*Check the capacity, Return the capacity*/
+extern size_t StackCapacity(Stack_t *stack)
+{
+	return stack->capacity;
+}
---
+++ ds/stack/stack_test.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include "stack.h"
+#include <string.h>
+
+int main() {
+
+    Stack_t *stack = NULL;
+    char *s = NULL;
+    int b;
+    int f;
+    char *last = NULL;
+    
+    s = (char*)calloc(100, sizeof(char));
+    if(!s)
+    {
+    	fprintf(stderr, "out of memory");
+    	exit(1);
+    }
+    
+    strcpy(s, "Hello world");
+    b = 5;
+    f = 3;
+    
+    stack = StackCreate(5);
+    
+    if(!(1 == StackIsEmpty(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    StackPush(stack, s);
+    
+    if(!(strcmp("Hello world", (char*)StackPeek(stack)) == 0))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    StackPush(stack, &b);
+    
+    if(!(5 == *(int*)StackPeek(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    if(!(0 == StackIsEmpty(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    StackPush(stack, &f); 
+    if(!(3 == *(int*)StackPeek(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }    
+    
+    if(!(3 == StackSize(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    if(!(5 == StackCapacity(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    StackPop(stack);
+    
+    if(!(2 == StackSize(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    if(!(5 == *(int*)StackPeek(stack)))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+    
+    last = (char*)calloc(100, sizeof(char));
+    if(!last)
+    {
+    	fprintf(stderr, "out of memory");
+    	exit(1);
+    }
+    
+    strcpy(last, "BYe byeeee Dumbeldore");
+    StackPush(stack, last);
+    
+    if(!(strcmp("BYe byeeee Dumbeldore", (char*)StackPeek(stack)) == 0))
+    {
+    	printf("fail in %d\n", __LINE__);
+    }
+	
+	free(last);
+	free(s);
+	StackDestroy(stack);	
+    
+   return 0;
+}
+
+
