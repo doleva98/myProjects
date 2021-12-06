c07a9dc5bb60b809654ff2a0581e1ec76e69a1a7
dolev <dolev.arev@gmail.com>
2021-12-06 17:25:36 +0200

    jh

---
+++ ds/bst/bst.h
@@ -0,0 +1,42 @@
+#ifndef _BST_H_
+#define _BST_H_
+
+typedef int (*compare_func_t)(const void *new_elem, const void *curr_elem, const void *param);
+typedef int (*match_func_t)(const void *data, const void *param);
+typedef int (*action_func_t)(void *data, const void *param);
+
+typedef struct BST bst_t;
+typedef struct BSTNode bst_node_t;
+
+typedef struct
+{
+    bst_t *bst;
+    bst_node_t *node;
+} bst_iter_t;
+
+extern bst_t *BstCreate(compare_func_t cmp_func, const void *param);
+
+extern void BstDestroy(bst_t *bst);
+
+extern size_t BstSize(bst_t *bst);
+
+extern int BstIsEmpty(bst_t *bst);
+
+extern void BstInsert(bst_t *bst, const void *data);
+
+extern void BstRemove(bst_t *bst, const void *data);
+
+extern bst_iter_t BstBegin(bst_t *bst);
+extern bst_iter_t BstEnd(bst_t *bst);
+extern bst_iter_t BstIterNext(bst_iter_t iter);
+extern bst_iter_t BstIterPrev(bst_iter_t iter);
+
+extern int BstIterIsEqual(bst_iter_t iter1, bst_iter_t iter2);
+
+extern void *BstIterGetData(bst_iter_t iter);
+
+extern bst_iter_t BstFind(bst_iter_t from, bst_iter_t to, match_func_t is_match_func, const void *param);
+
+extern int BstForEach(bst_iter_t from, bst_iter_t to, action_func_t action_func, const void *param);
+
+#endif
--- ds/dlinkedlist/doubly_linked_list.h
+++ ds/dlinkedlist/doubly_linked_list.h
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 
-
 typedef struct DList dlist_t;
 typedef struct DListNode dnode_t;
 
@@ -12,10 +11,9 @@ typedef int (*match_func_t)(void *data, void *param);
 
 typedef struct
 {
-	dlist_t *list;
-	dnode_t* node;
+  dlist_t *list;
+  dnode_t *node;
 } dlist_iter_t;
-
 
 /* Create new list */
 extern dlist_t *DListCreate();
@@ -32,12 +30,11 @@ extern size_t DListCount(dlist_t *list);
 extern dlist_iter_t DListIterNext(dlist_iter_t iter);
 extern dlist_iter_t DListIterPrev(dlist_iter_t iter);
 extern int DListIterIsEqual(dlist_iter_t iter1, dlist_iter_t iter2);
-extern void* DListIterGetData(dlist_iter_t iter);
+extern void *DListIterGetData(dlist_iter_t iter);
 
 dlist_iter_t DListFind(dlist_iter_t from, dlist_iter_t to, match_func_t is_match_func, void *param);
 int DListForEach(dlist_iter_t from, dlist_iter_t to, action_func_t action_func, void *param);
-  size_t DListCountForEach(dlist_t *list);
-
+size_t DListCountForEach(dlist_t *list);
 
 /* ----------- UPDATE -------------- */
 size_t DListSize(dlist_t *list);
@@ -47,15 +44,13 @@ dlist_iter_t DListPushBack(dlist_t *list, const void *data);
 void *DListPopFront(dlist_t *list);
 void *DListPopBack(dlist_t *list);
 
-/* Cuts & pastes the range between begin and end into the list before 
+/* Cuts & pastes the range between begin and end into the list before
    the element referred to by where.
    Returns iterator to the last spliced element.
    O(1) */
 
-dlist_iter_t DListSplice(dlist_iter_t where, 
-                         dlist_iter_t begin, 
+dlist_iter_t DListSplice(dlist_iter_t where,
+                         dlist_iter_t begin,
                          dlist_iter_t end);
-
-
 
 #endif
