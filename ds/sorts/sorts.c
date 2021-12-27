1aea03597be6511389ecd33f8f249da4a4328251
dolev <dolev.arev@gmail.com>
2021-12-27 16:07:55 +0200

    g

--- ds/sorts/sorts.c
+++ ds/sorts/sorts.c
@@ -22,20 +22,20 @@ void *RecBinarySearch(void *base, size_t nmemb, size_t size, cmp_func_t compare,
 static void *MergeSortHelper(void *base, size_t size, cmp_func_t compare, size_t l, size_t r)
 {
 	size_t m = 0;
-	if (r <= l)
+	if (l < r)
 	{
-		return NULL;
-	}
-	m = l + (r - l) / 2;
+		m = l + (r - l) / 2;
 
-	MergeSortHelper(base, size, compare, l, m);
-	MergeSortHelper(base, size, compare, m + 1, r);
-	return Merge(base, size, compare, l, m, r);
+		MergeSortHelper(base, size, compare, l, m);
+		MergeSortHelper(base, size, compare, m + 1, r);
+		Merge(base, size, compare, l, m, r);
+	}
+	return base;
 }
 
 static void *Merge(void *base, size_t size, cmp_func_t compare, size_t l, size_t m, size_t r)
 {
-	size_t i, j, k;
+	size_t i = 0, j = 0, k = l;
 	size_t n1 = m - l + 1;
 	size_t n2 = r - m;
 
@@ -48,13 +48,46 @@ static void *Merge(void *base, size_t size, cmp_func_t compare, size_t l, size_t
 		return NULL;
 	}
 
-	memcpy(left_arr, base, size * n1);
+	memcpy(left_arr, (void *)((size_t)base + (l * size)), size * n1);
 
-	right_arr = malloc(size * n1);
+	right_arr = malloc(size * n2);
 	if (!right_arr)
 	{
+		free(left_arr);
 		return NULL;
 	}
 
-	memcpy(right_arr, (char *)base + (size * n1), size * n1);
+	memcpy(right_arr, (void *)((size_t)base + (size * (n1 + l))), size * n2);
+
+	while (i < n1 && j < n2)
+	{
+		if (compare((void *)((size_t)left_arr + (i * size)), (void *)((size_t)right_arr + (j * size))) < 0)
+		{
+			memcpy((void *)((size_t)base + (k * size)), (void *)((size_t)left_arr + (i * size)), size);
+			++i;
+		}
+		else
+		{
+			memcpy((void *)((size_t)base + (k * size)), (void *)((size_t)right_arr + (j * size)), size);
+			++j;
+		}
+		++k;
+	}
+
+	while (i < n1)
+	{
+		memcpy((void *)((size_t)base + (k * size)), (void *)((size_t)left_arr + (i * size)), size);
+		++i;
+		++k;
+	}
+
+	while (j < n2)
+	{
+		memcpy((void *)((size_t)base + (k * size)), (void *)((size_t)right_arr + (j * size)), size);
+		++j;
+		++k;
+	}
+	free(left_arr);
+	free(right_arr);
+	return base;
 }
--- ds/sorts/test_sorts.c
+++ ds/sorts/test_sorts.c
@@ -1,9 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "sorts.h"
+
+static void PrintArray(int *a, size_t len);
+static int CompareInt(const void *n1, const void *n2);
+static void CheckIfOrdered(int *a, size_t len);
+static void CheckMergeSort();
+
 int main()
 {
-	
-	
-
+	CheckMergeSort();
 	return 0;
+}
+
+static void CheckMergeSort()
+{
+	int a[] = {2, 5, 3, 0, 100, 9898, -8, 86};
+	int b[20];
+	size_t i = 0;
+
+	srand(time(NULL));
+	puts("before sorting");
+	PrintArray(a, sizeof(a) / sizeof(*a));
+	puts("after sorting");
+	MergeSort(a, sizeof(a) / sizeof(*a), sizeof(*a), CompareInt);
+	PrintArray(a, sizeof(a) / sizeof(*a));
+	CheckIfOrdered(a, sizeof(a) / sizeof(*a));
+
+	for (i = 0; i < sizeof(b) / sizeof(*b); ++i)
+	{
+		b[i] = rand();
+	}
+	MergeSort(b, sizeof(b) / sizeof(*b), sizeof(*b), CompareInt);
+	CheckIfOrdered(b, sizeof(b) / sizeof(*b));
+}
+
+static void PrintArray(int *a, size_t len)
+{
+	size_t i = 0;
+	for (i = 0; i < len; ++i)
+	{
+		printf("%d\n", a[i]);
+	}
+}
+
+static int CompareInt(const void *n1, const void *n2)
+{
+	return *(int *)n1 - *(int *)n2;
+}
+
+static void CheckIfOrdered(int *a, size_t len)
+{
+	size_t i = 0;
+	for (i = 1; i < len; ++i)
+	{
+		if (a[i] < a[i - 1])
+		{
+			printf("fail in %d\n", __LINE__);
+		}
+	}
 }
