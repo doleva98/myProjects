2151033b70eac2a8ceaa4e6d33d90f29aecc4aa0
dolev <dolev.arev@gmail.com>
2022-03-24 14:51:07 +0200

    g

--- fs/java/projects/src/il/co/ilrd/networking/PingPongClientBroadcast.java
+++ fs/java/projects/src/il/co/ilrd/networking/PingPongClientBroadcast.java
@@ -1,5 +1,82 @@
 package il.co.ilrd.networking;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import il.co.ilrd.utility.ColorsFont;
+
 public class PingPongClientBroadcast {
-    
+    private static Scanner scan = new Scanner(System.in);
+
+    public PingPongClientBroadcast() {
+
+        try (Scanner scan = new Scanner(System.in);
+                DatagramSocket ds = new DatagramSocket();
+                Scanner scan2 = new Scanner(System.in)) {
+
+            List<Integer> listOfPorts = new ArrayList<>();
+            System.out.println("enter port numbers or -1 to quit");
+            int portNumber = scan.nextInt();
+            while (portNumber != -1) {
+                listOfPorts.add(portNumber);
+                portNumber = scan.nextInt();
+            }
+            InetAddress ip = InetAddress.getLocalHost();
+            byte[] buf = null;
+            byte[] receive = new byte[65535];
+
+            DatagramPacket outputDatagramPacket = null;
+            DatagramPacket inputDatagramPacket = null;
+
+            System.out.println(ColorsFont.ANSI_CYAN + "********" + ColorsFont.ANSI_RESET);
+            System.out.println("send ping or pong");
+            String line = "";
+
+            line = scan2.nextLine();
+
+            while (line.equals("ping") || line.equals("pong")) {
+                buf = line.getBytes();
+                for (int port : listOfPorts) {
+                    outputDatagramPacket = new DatagramPacket(buf, buf.length, ip, port);
+                    ds.send(outputDatagramPacket);
+
+                }
+
+                for (int port : listOfPorts) {
+
+                    inputDatagramPacket = new DatagramPacket(receive, receive.length);
+                    ds.receive(inputDatagramPacket);
+                    System.out.println("received a packet from the server with port " + port);
+                    line = ByteArrayToString.byteArrayToString(receive);
+                    if (line.equals("ping")) {
+                        System.out.println("client got ping");
+                    } else if (line.equals("pong")) {
+                        System.out.println("client got pong");
+                    }
+                }
+
+                System.out.println(ColorsFont.ANSI_CYAN + "********" + ColorsFont.ANSI_RESET);
+                System.out.println("send ping or pong");
+                line = scan2.nextLine();
+            }
+            System.out.println("exiting");
+            buf = "exit".getBytes();
+            for (int port : listOfPorts) {
+                outputDatagramPacket = new DatagramPacket(buf, buf.length, ip, port);
+                ds.send(outputDatagramPacket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        PingPongClientBroadcast client = new PingPongClientBroadcast();
+    }
 }
--- fs/java/projects/src/il/co/ilrd/networking/PingPongServerBroadcast.java
+++ fs/java/projects/src/il/co/ilrd/networking/PingPongServerBroadcast.java
@@ -1,5 +1,63 @@
 package il.co.ilrd.networking;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Scanner;
+
+import il.co.ilrd.utility.ColorsFont;
+
 public class PingPongServerBroadcast {
-    
+    public PingPongServerBroadcast(int port) {
+        try (DatagramSocket ds = new DatagramSocket(port);) {
+            byte[] receive = new byte[65535];
+            String line = "";
+            DatagramPacket inputDatagramPacket = null;
+            DatagramPacket outputDatagramPacket = null;
+
+            String output = "";
+            byte[] buf = null;
+
+            System.out.println("server started");
+
+            while (!line.equals("exit")) {
+                System.out.println("waiting for a packet");
+                System.out.println(ColorsFont.ANSI_PURPLE + "********" + ColorsFont.ANSI_RESET);
+
+                inputDatagramPacket = new DatagramPacket(receive, receive.length);
+                ds.receive(inputDatagramPacket);
+
+                System.out.println("server got a packet");
+                line = ByteArrayToString.byteArrayToString(receive);
+
+                if (line.equals("ping")) {
+                    System.out.println("server got ping");
+                    System.out.println("sends pong");
+                    output = "pong";
+                } else if (line.equals("pong")) {
+                    System.out.println("server got pong");
+                    System.out.println("sends ping");
+                    output = "ping";
+                } else {
+                    System.out.println("exiting");
+                    break;
+                }
+                buf = output.getBytes();
+                outputDatagramPacket = new DatagramPacket(buf, buf.length, inputDatagramPacket.getAddress(),
+                        inputDatagramPacket.getPort());
+                ds.send(outputDatagramPacket);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("enter port number");
+            @SuppressWarnings("unused")
+            PingPongServerBroadcast server = new PingPongServerBroadcast(scan.nextInt());
+        }
+    }
 }
