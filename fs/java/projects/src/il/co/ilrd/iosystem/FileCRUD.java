bb0bbaeb297002a89c9a06f517936942c46a6a2c
dolev <dolev.arev@gmail.com>
2022-03-30 08:17:25 +0300

    g

--- fs/java/projects/src/il/co/ilrd/iosystem/FileCRUD.java
+++ fs/java/projects/src/il/co/ilrd/iosystem/FileCRUD.java
@@ -12,21 +12,18 @@ import java.util.List;
 import java.util.Objects;
 
 class FileCRUD implements CRUD<Integer, String> {
-    private String path = null;
+    private final String path;
     private BufferedWriter bw = null;
-    private BufferedReader br = null;
 
     public FileCRUD(String path) throws IOException {
         Objects.requireNonNull(path);
         this.path = path;
         bw = new BufferedWriter(new FileWriter(path));
-        br = new BufferedReader(new FileReader(path));
     }
 
     @Override
     public void close() throws Exception {
         bw.close();
-        br.close();
     }
 
     @Override
@@ -46,27 +43,25 @@ class FileCRUD implements CRUD<Integer, String> {
     public void update(Integer key, String data) throws IOException, ClassNotFoundException {
         List<String> list = Files.readAllLines(Paths.get(path));
         list.remove((int) key);
-        BufferedWriter bw1 = new BufferedWriter(new FileWriter(path));
-        bw1.close();
-        for (int i = 0; i < key; ++i) {
-            create(list.get(i));
+        list.add(key, data);
+        cleanFile();
+        for (String s : list) {
+            create(s);
         }
-        create(data);
-        for (int i = key; i < list.size(); ++i) {
-            create(list.get(i));
-        }
-
     }
 
     @Override
     public void delete(Integer key) throws IOException, ClassNotFoundException {
         List<String> list = Files.readAllLines(Paths.get(path));
         list.remove((int) key);
-        BufferedWriter bw1 = new BufferedWriter(new FileWriter(path));
-        bw1.close();
+        cleanFile();
         for (String str : list) {
             create(str);
         }
     }
 
+    private void cleanFile() throws IOException {
+        BufferedWriter bw1 = new BufferedWriter(new FileWriter(path));
+        bw1.close();
+    }
 }
