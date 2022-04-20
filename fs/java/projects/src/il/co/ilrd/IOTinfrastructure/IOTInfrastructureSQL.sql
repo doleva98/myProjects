ade4072473a929980c28d9a82a6e321493c48f6f
dolev <dolev.arev@gmail.com>
2022-04-20 12:34:43 +0300

    g

--- fs/java/projects/src/il/co/ilrd/IOTinfrastructure/IOTInfrastructureSQL.sql
+++ fs/java/projects/src/il/co/ilrd/IOTinfrastructure/IOTInfrastructureSQL.sql
@@ -2,53 +2,53 @@ CREATE DATABASE IF NOT EXISTS IOTInfrastructure;
 USE IOTInfrastructure;
 CREATE TABLE IF NOT EXISTS Companies(
     companyID INT,
-    company_name VARCHAR(250),
-    contactID INT,
-    addressID INT,
+    company_name VARCHAR(250) NOT NULL,
+    contactID INT NOT NULL,
+    addressID INT NOT NULL,
     PRIMARY KEY(companyID),
-    FOREIGN KEY(addressID) REFERENCES Adresses(addressID)
+    FOREIGN KEY(addressID) REFERENCES Adresses(addressID),
+    FOREIGN KEY(contactID) REFERENCES ContactPerson(contactID)
 );
 CREATE TABLE IF NOT EXISTS Products(
     productID INT,
-    productname VARCHAR(250),
-    companyID INT,
+    productname VARCHAR(250) NOT NULL,
+    companyID INT NOT NULL,
     PRIMARY KEY(productID),
     FOREIGN KEY(companyID) REFERENCES Companies(companyID)
 );
 CREATE TABLE IF NOT EXISTS IOT(
     IOTID INT,
-    serialNum INT,
-    productID INT,
+    serialNum INT NOT NULL,
+    productID INT NOT NULL,
+    PRIMARY KEY(IOTID),
     FOREIGN KEY(productID) REFERENCES Products(productID)
 );
 CREATE TABLE IF NOT EXISTS IOTLog(
     logNumber INT,
-    IOTID INT,
-    logDate DATE,
-    logData VARCHAR(250),
+    IOTID INT NOT NULL,
+    logDate DATE DEFAULT GETDATE(),
+    logData VARCHAR(250) NOT NULL,
     PRIMARY KEY(logNumber),
     FOREIGN KEY(IOTID) REFERENCES IOT(IOTID)
 );
 CREATE TABLE IF NOT EXISTS Adresses(
     addressID INT,
-    zipcode INT,
-    city VARCHAR(250),
-    street VARCHAR(250),
+    zipcode INT NOT NULL,
+    city VARCHAR(250) NOT NULL,
+    street VARCHAR(250) NOT NULL,
     PRIMARY KEY(addressID)
 );
 CREATE TABLE IF NOT EXISTS Payments(
     payID INT,
-    companyID INT,
-    creditDetails VARCHAR(250),
+    companyID INT NOT NULL,
+    creditDetails VARCHAR(250) NOT NULL,
     PRIMARY KEY(payID),
     FOREIGN KEY(companyID) REFERENCES Companies(companyID)
 );
 CREATE TABLE IF NOT EXISTS ContactPerson(
     contactID INT,
-    companyID INT,
-    phoneNumber INT,
-    contactPersonName VARCHAR(250),
-    email VARCHAR(250),
+    phoneNumber INT NOT NULL,
+    contactPersonName VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
     PRIMARY KEY(contactID),
-    FOREIGN KEY(contactID, companyID) REFERENCES Companies(contactID, companyID)
 );
