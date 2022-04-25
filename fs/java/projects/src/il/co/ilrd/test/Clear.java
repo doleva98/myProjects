21326f100ba88cc99d060cba8f50f47a055a5a57
dolev <dolev.arev@gmail.com>
2022-04-25 16:01:19 +0300

    g

--- fs/java/projects/src/il/co/ilrd/test/Clear.java
+++ fs/java/projects/src/il/co/ilrd/test/Clear.java
@@ -2,8 +2,35 @@ package il.co.ilrd.test;
 
 import java.util.Arrays;
 
+interface s {
+    default public void f() {
+        System.out.println("hello");
+    }
+}
+
+abstract class A implements s {
+
+    public void c() {
+        f();
+    }
+}
+
+class B extends A implements s {
+    public B() {
+        super.c();
+    }
+
+    @Override
+    public void f() {
+        System.out.println("a");
+    }
+}
+
 class Main {
+
     public static void main(String[] args) {
-System.out.println(52%4);
+        B b = new B();
+        /*  b.f();
+        A a = new A(); */
     }
 }
